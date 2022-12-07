1de5801bdaddf5129160210bb6749a697bc3a343
psc <ppsschang@gmail.com>
2022-12-08 07:33:35 +0900

    chapter1 volumeCredits 우변을 함수로 추출

--- src/ch1/statement.ts
+++ src/ch1/statement.ts
@@ -33,12 +33,11 @@ function statement(invoice: Iinvoice, plays: Iplays) {
     const format = new Intl.NumberFormat("es-US",
         { style: "currency", currency: "USD", minimumFractionDigits: 2}).format
 
-
-    function amountFor(aPerformance: { playId: string; audience: number }) { //명확한 이름으로 변경
+    function amountFor(aPerformance: { playId: string; audience: number }) {
         //인터페이스에 있는 타입을 사용 할 순 없나?
-        let result = 0 //명확한 이름으로 변경
+        let result = 0
 
-        switch (playFor(aPerformance).type) { //play 를 playFor() 호출로 변경
+        switch (playFor(aPerformance).type) {
             case "tragedy": {
                 result = 40000
                 if (aPerformance.audience > 30) {
@@ -54,23 +53,30 @@ function statement(invoice: Iinvoice, plays: Iplays) {
             }
                 break;
             default:
-                throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`) //play 를 playFor() 호출로 변경
+                throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`)
         }
-        return result; // 함수안에서 값이 바뀌는 변수를 반환
+        return result;
     }
 
     function playFor(perf: { playId: string; audience: number }) {
         return plays[perf.playId];
     }
 
-    for (let perf of invoice.performances) {
-        //포인트를 적립
+    function volumeCreditsFor(perf: { playId: string; audience: number }) {
+        volumeCredits = 0
         volumeCredits += Math.max(perf.audience - 30, 0)
         //희극 관객 5명마다 추가 포인트 제공
-        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5) // playFor 변수를 인라인화
+        if ("comedy" === playFor(perf).type)
+            volumeCredits += Math.floor(perf.audience / 5)
+        return volumeCredits
+    }
+
+    for (let perf of invoice.performances) {
+        //포인트를 적립
+        volumeCredits += volumeCreditsFor(perf) // 추출한 함수를 이용하여 값을 누적
         //청구 내역 출력
         result += `${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석)\n` // thisAmount 변수를 인라인화
-        totalAmount += amountFor(perf) // thisAmount 변수를 인라인화
+        totalAmount += amountFor(perf)
     }
 
     result += `총액: ${format(totalAmount/100)}\n`
