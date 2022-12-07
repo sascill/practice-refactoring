7be9a94e4688d096d220ed866b08a2ece25d960c
psc <ppsschang@gmail.com>
2022-12-08 07:38:39 +0900

    chapter1 임시 변수 였던 format 을 함수 호출로 대체

--- src/ch1/statement.ts
+++ src/ch1/statement.ts
@@ -30,8 +30,11 @@ function statement(invoice: Iinvoice, plays: Iplays) {
     let totalAmount = 0
     let volumeCredits = 0
     let result = `청구 내역 (고객명: ${invoice.customer})\n`
-    const format = new Intl.NumberFormat("es-US",
-        { style: "currency", currency: "USD", minimumFractionDigits: 2}).format
+
+    function format(aNumber: number) {
+        return new Intl.NumberFormat("es-US",
+            {style: "currency", currency: "USD", minimumFractionDigits: 2}).format(aNumber)
+    }
 
     function amountFor(aPerformance: { playId: string; audience: number }) {
         //인터페이스에 있는 타입을 사용 할 순 없나?
@@ -73,13 +76,13 @@ function statement(invoice: Iinvoice, plays: Iplays) {
 
     for (let perf of invoice.performances) {
         //포인트를 적립
-        volumeCredits += volumeCreditsFor(perf) // 추출한 함수를 이용하여 값을 누적
+        volumeCredits += volumeCreditsFor(perf)
         //청구 내역 출력
         result += `${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석)\n` // thisAmount 변수를 인라인화
         totalAmount += amountFor(perf)
     }
 
-    result += `총액: ${format(totalAmount/100)}\n`
+    result += `총액: ${format(totalAmount/100)}\n` //임시 변수 였던 format 을 함수 호출로 대체
     result += `적립 포인트: ${volumeCredits}점\n`
 
     return result
