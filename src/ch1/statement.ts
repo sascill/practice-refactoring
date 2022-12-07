622646dc0f40e7ca534e2021941197d06b61df5f
psc <ppsschang@gmail.com>
2022-12-08 07:07:16 +0900

    chapter1 함수 추출하기 (switch)

--- src/ch1/statement.ts
+++ src/ch1/statement.ts
@@ -33,9 +33,10 @@ function statement(invoice: Iinvoice, plays: Iplays) {
     const format = new Intl.NumberFormat("es-US",
         { style: "currency", currency: "USD", minimumFractionDigits: 2}).format
 
-    for (let perf of invoice.performances) {
-        const play = plays[perf.playId]
-        let thisAmount = 0
+
+    function amountFor(play: { name: string; type: string }, perf: { playId: string; audience: number }) { //값이 바뀌지 않는 변수는 매개변수로 전달
+        //인터페이스에 있는 타입을 사용 할 순 없나?
+        let thisAmount = 0 //변수 초기화
 
         switch (play.type) {
             case "tragedy": {
@@ -44,17 +45,24 @@ function statement(invoice: Iinvoice, plays: Iplays) {
                     thisAmount += 1000 * (perf.audience - 30)
                 }
             }
-            break;
+                break;
             case "comedy": {
                 thisAmount = 30000
                 if (perf.audience > 20) {
                     thisAmount += 10000 + 500 * (perf.audience - 20)
                 }
             }
-            break;
+                break;
             default:
                 throw new Error(`알 수 없는 장르: ${play.type}`)
         }
+        return thisAmount; // 함수안에서 값이 바뀌는 변수를 반환
+    }
+
+    for (let perf of invoice.performances) {
+        const play = plays[perf.playId]
+        let thisAmount = 0
+        thisAmount = amountFor(play, perf);
         //포인트를 적립
         volumeCredits += Math.max(perf.audience - 30, 0)
         //희극 관객 5명마다 추가 포인트 제공
