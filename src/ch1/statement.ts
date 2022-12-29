3e66a81f251d23e3a62d5589529f5ebca29f7735
psc <ppsschang@gmail.com>
2022-12-29 15:37:45 +0900

    chapter1 volumeCreditsFor 함수 중간데이터로 변경

--- src/ch1/statement.ts
+++ src/ch1/statement.ts
@@ -1,17 +1,20 @@
 "use strict"
-interface IInvoice {
-    customer: string
+
+interface IPerformance {
+    playId: string,
+    audience: number,
+    amount: number,
+    volumeCredits: number,
+    play: {
+        name: string,
+        type: string
+    }
 }
 
-interface IPerformances extends IInvoice {
-    performances: {
-        playId: string,
-        audience: number,
-        play: {
-            name: string,
-            type: string
-        }
-    }[]
+interface IInvoice {
+    customer: string,
+    performances: IPerformance[]
+
 }
 
 interface IPlays {
@@ -40,6 +43,8 @@ function statement(invoice:IInvoice, plays: IPlays) {
             {
                 playId:'',
                 audience: 0,
+                amount: 0,
+                volumeCredits: 0,
                 play: {
                     name: '',
                     type: ''
@@ -49,50 +54,25 @@ function statement(invoice:IInvoice, plays: IPlays) {
     }
 
     statementData.customer = invoice.customer
-    // @ts-ignore
     statementData.performances = invoice.performances.map(enrichPerformance)
     return renderPlainText(statementData, plays)
 
-    function enrichPerformance(aPerformance: { playId: string; audience: number }) {
+    function enrichPerformance(aPerformance: IPerformance) {
         const result = Object.assign({}, aPerformance)
-        // @ts-ignore
         result.play = playFor(result)
+        result.amount = amountFor(result)
+        result.volumeCredits = volumeCreditsFor(result)
         return result
     }
 
-    function playFor(perf: { playId: string; audience: number }) {
+    function playFor(perf: IPerformance) {
         return plays[perf.playId];
     }
-}
-function renderPlainText(data: IPerformances, plays: IPlays) {
-    let result = `청구 내역 (고객명: ${data.customer})\n`
 
-    for (let perf of data.performances) {
-        //청구 내역 출력
-        result += `${perf.play?.name}: ${usd(amountFor(perf))} (${perf.audience}석)\n` // thisAmount 변수를 인라인화
-    }
-
-    result += `총액: ${usd(totalAmount())}\n`
-    result += `적립 포인트: ${totalVolumeCredits()}점\n`
-
-    return result
-
-    function usd(aNumber: number) {
-        return new Intl.NumberFormat("es-US",
-            {style: "currency", currency: "USD", minimumFractionDigits: 2}).format(aNumber/100)
-    }
-
-    function amountFor(aPerformance: {
-            playId: string,
-            audience: number
-            play?: {
-                name:string,
-                type:string
-            }
-        }) {
+    function amountFor(aPerformance: IPerformance) {
         //인터페이스에 있는 타입을 사용 할 순 없나?
         let result = 0
-        switch (aPerformance.play?.type) {
+        switch (aPerformance.play.type) {
             case "tragedy": {
                 result = 40000
                 if (aPerformance.audience > 30) {
@@ -108,12 +88,12 @@ function renderPlainText(data: IPerformances, plays: IPlays) {
             }
                 break;
             default:
-                throw new Error(`알 수 없는 장르: ${aPerformance.play?.type}`)
+                throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`)
         }
         return result;
     }
 
-    function volumeCreditsFor(aPerformance: { playId: string; audience: number, play: { name: string, type:string} }) {
+    function volumeCreditsFor(aPerformance: IPerformance) {
         let result = 0
         result += Math.max(aPerformance.audience - 30, 0)
         //희극 관객 5명마다 추가 포인트 제공
@@ -121,11 +101,32 @@ function renderPlainText(data: IPerformances, plays: IPlays) {
             result += Math.floor(aPerformance.audience / 5)
         return result
     }
+}
+
+function renderPlainText(data: IInvoice, plays: IPlays) {
+    let result = `청구 내역 (고객명: ${data.customer})\n`
+
+    for (let perf of data.performances) {
+        //청구 내역 출력
+        result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n` // thisAmount 변수를 인라인화
+    }
+
+    result += `총액: ${usd(totalAmount())}\n`
+    result += `적립 포인트: ${totalVolumeCredits()}점\n`
+
+    return result
+
+    function usd(aNumber: number) {
+        return new Intl.NumberFormat("es-US",
+            {style: "currency", currency: "USD", minimumFractionDigits: 2}).format(aNumber/100)
+    }
+
+
 
     function totalVolumeCredits(): number {
         let result = 0
         for (let perf of data.performances) { //값 누적 로직을 별도 for 문으로 분리
-            result += volumeCreditsFor(perf)
+            result += perf.volumeCredits
         }
         return result
     }
@@ -133,7 +134,7 @@ function renderPlainText(data: IPerformances, plays: IPlays) {
     function totalAmount(): number {
         let result = 0
         for (let perf of data.performances) {
-            result += amountFor(perf);
+            result += perf.amount;
         }
         return result
     }
